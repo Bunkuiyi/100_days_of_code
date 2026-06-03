0cebe63eab59670a8a3777774f1e1c82b6410aba
Bunkuiyi <bunkuiyi@outlook.com>
2026-06-03 15:18:41 +0100

    Day 1 Project - inputs, printing string manipulation, variables, variable naming

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -1,1 +1,8 @@
-# Write your code below this line 👇
+# print(len(input("What is your name?")))
+print("Welcome to the Band Name Generator")
+
+place_live = input("Where do you live?\n")
+
+first_pet = input("What was the name of your first pet?\n")
+
+print("Why don't you try " + place_live + " " + first_pet + " as a band name?")

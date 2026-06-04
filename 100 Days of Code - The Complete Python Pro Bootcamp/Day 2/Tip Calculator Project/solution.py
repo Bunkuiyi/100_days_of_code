fd544d20eda206e9f0681d55985f869e7ed7685b
Bunkuiyi <bunkuiyi@outlook.com>
2026-06-04 19:37:41 +0100

    Day 2 Project - f-strings, data types, type conversion, mathematical operations, type error checking

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Tip Calculator Project/solution.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Tip Calculator Project/solution.py
@@ -2,10 +2,10 @@ print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
-tip_as_percent = tip / 100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / people
+tip_as_percent = tip / 100 # Turning into a percentage
+total_tip_amount = bill * tip_as_percent # Calculating amount of tip from total bill
+total_bill = bill + total_tip_amount # Adding the tip amount onto original bill to give final bill number
+bill_per_person = total_bill / people # Working out how much each person pays by dividing final bill number by number of people paying
 final_amount = round(bill_per_person, 2)
 print(f"Each person should pay: ${final_amount}")
 
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Tip Calculator Project/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Tip Calculator Project/task.py
@@ -1,6 +1,12 @@
 print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
+bill = float(input("What was the total bill? £"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
+tip_percentage = tip / 100
+tip_amount = bill * tip_percentage
+total_bill = bill + tip_amount
+final_bill = total_bill / people
+total = round(final_bill, 2)
 
+print(f"Each person should pay £{total}")

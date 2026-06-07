65a5a3f96f25b7efae580cd196a3635c5f7a04a0
Bunkuiyi <bunkuiyi@outlook.com>
2026-06-07 22:30:43 +0100

    Day 5 project - loops, for loops, for loops with range, sum, max

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 5/For Loops/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 5/For Loops/task.py
@@ -1,1 +1,16 @@
-fruits = ["Apple", "Peach", "Pear"]
+# fruits = ["Apple", "Peach", "Pear"]
+#
+# # loop executes same line of code multiple times
+#
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " pie")
+
+import random
+
+numbers = [random.randint(0, 100)]
+
+for number in numbers:
+    print(number)
+    if 0 <= number <= 50:
+        print("This is a test. ")
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 5/Highest Score/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 5/Highest Score/task.py
@@ -1,2 +1,21 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
-print(range(1, 10))
+
+total_scores = sum(student_scores)
+
+sum = 0
+for score in student_scores:
+    sum += score
+
+print(sum)
+
+print(max(student_scores))
+
+max = 0
+for highest_score in student_scores:
+    if highest_score > max :
+       max = highest_score
+
+
+print(max)
+
+

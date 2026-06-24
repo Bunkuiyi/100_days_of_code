a5b7679effea69913bc5802f0bc02c96112ce209
Bunkuiyi <bunkuiyi@outlook.com>
2026-06-24 19:14:55 +0100

    Day 16 - Object Oriented Programming part 1

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/Where are the rest of the days/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16-100/Where are the rest of the days/main.py
@@ -2,3 +2,6 @@
 # Instead of using templates that I have created for you.
 # It will be another step in your journey as a developer!
 # But don't worry, I will explain how to do everything in the video tutorials on Udemy.
+
+# An object is a way of combining some piece of data and some functionality all together in the same thing
+# Class
---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+#Object #Class
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("aquamarine4")
+timmy.forward(100)
+
+my_screen = Screen()
+                #Attribute
+print(my_screen.canvheight)
+          #Method
+my_screen.exitonclick()
